4826cae6a9f0317d861454b7191abdc1959f0e77
Simon Morgan <sjm@sjm.io>
2014-05-11 15:42:40 +0100

    Read from stdin.

--- src/com/drguildo/dailyprogrammer/easy/Blackjack.java
+++ src/com/drguildo/dailyprogrammer/easy/Blackjack.java
@@ -91,7 +91,7 @@ final class Blackjack {
   }
 
   public static void main(String[] args) {
-    Scanner scanner = new Scanner("10");
+    Scanner scanner = new Scanner(System.in);
     int n = scanner.nextInt();
     scanner.close();
 
