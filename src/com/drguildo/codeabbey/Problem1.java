dea646654bc5f81f6eda04d8fae8970519b7e744
Simon Morgan <sjm@sjm.io>
2015-02-18 16:26:58 +0000

    CodeAbbey skeleton and solution to problem #1.

---
+++ src/com/drguildo/codeabbey/Problem1.java
@@ -0,0 +1,14 @@
+package com.drguildo.codeabbey;
+
+import java.util.*;
+
+public class Problem1 {
+  public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+    int a, b;
+    a = in.nextInt();
+    b = in.nextInt();
+    System.out.println(a + b);
+    in.close();
+  }
+}
---
+++ src/com/drguildo/codeabbey/Skeleton.java
@@ -0,0 +1,10 @@
+package com.drguildo.codeabbey;
+
+import java.util.*;
+
+public class Skeleton {
+  public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+    in.close();
+  }
+}
